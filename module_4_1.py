516c71bc7cd1fbbf0355b220470f7bbfaae21cf5
ShumsAn <shum-sol@outlook.com>
2024-08-05 14:11:59 +0300

    Add files via upload
    Домашняя работа по уроку "Модули и пакеты"

---
+++ module_4_1.py
@@ -0,0 +1,13 @@
+from fake_math import divide as dv_fake
+from true_math import divide as dv_true
+
+
+
+result1 = dv_fake(69, 3)
+result2 = dv_fake(3, 0)
+result3 = dv_true(49, 7)
+result4 = dv_true(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
---
+++ true_math.py
@@ -0,0 +1,12 @@
+
+from math import inf
+
+def divide(first, second):
+    if second == 0:
+        return inf
+
+    else:
+        return first / second
+
+# res1 = divide(123,0)
+# print(res1)
